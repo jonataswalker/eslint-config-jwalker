8db4decbfe137db73c45a94c01234da39fc54c89
Jonatas Walker <jonataswalker@gmail.com>
2022-07-19 09:41:02 -0300

    replace node plugin

--- node.js
+++ node.js
@@ -1,21 +1,21 @@
 module.exports = {
-    extends: ['plugin:node/recommended'],
+    extends: ['plugin:n/recommended'],
     rules: {
-        'node/prefer-promises/fs': 'error',
-        'node/prefer-promises/dns': 'error',
-        'node/prefer-global/url': ['error', 'always'],
-        'node/prefer-global/buffer': ['error', 'always'],
-        'node/prefer-global/console': ['error', 'always'],
-        'node/prefer-global/process': ['error', 'always'],
-        'node/file-extension-in-import': ['error', 'always'],
-        'node/prefer-global/url-search-params': ['error', 'always'],
-        'node/exports-style': ['error', 'module.exports'],
+        'n/prefer-promises/fs': 'error',
+        'n/prefer-promises/dns': 'error',
+        'n/prefer-global/url': ['error', 'always'],
+        'n/prefer-global/buffer': ['error', 'always'],
+        'n/prefer-global/console': ['error', 'always'],
+        'n/prefer-global/process': ['error', 'always'],
+        'n/file-extension-in-import': ['error', 'always'],
+        'n/prefer-global/url-search-params': ['error', 'always'],
+        'n/exports-style': ['error', 'module.exports'],
     },
     overrides: [
         {
             files: ['*.ts'],
             rules: {
-                'node/file-extension-in-import': ['error', 'never'],
+                'n/file-extension-in-import': ['error', 'never'],
             },
         },
     ],
