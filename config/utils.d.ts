1297fa5a84aa2d4ea6cb64da1a8a70c8c89fbd5b
Jonatas Walker <jonataswalker@gmail.com>
2024-02-29 16:18:17 -0300

    fix/add types

---
+++ config/utils.d.ts
@@ -0,0 +1,3 @@
+export const tsRecommendedTypeCheckedRules: import('@typescript-eslint/utils').TSESLint.ClassicConfig.RulesRecord;
+export const tsStrictTypeCheckedRules: import('@typescript-eslint/utils').TSESLint.ClassicConfig.RulesRecord;
+export const tsDisableTypeCheckedRules: import('@typescript-eslint/utils').TSESLint.ClassicConfig.RulesRecord;
--- config/utils.js
+++ config/utils.js
@@ -2,8 +2,11 @@ import tsPlugin from '@typescript-eslint/eslint-plugin'
 
 const { configs } = tsPlugin
 
+/** @type {import('@typescript-eslint/utils').TSESLint.ClassicConfig.RulesRecord} */
 export const tsRecommendedTypeCheckedRules = configs['recommended-type-checked'].rules
 
+/** @type {import('@typescript-eslint/utils').TSESLint.ClassicConfig.RulesRecord} */
 export const tsStrictTypeCheckedRules = configs['strict-type-checked'].rules
 
+/** @type {import('@typescript-eslint/utils').TSESLint.ClassicConfig.RulesRecord} */
 export const tsDisableTypeCheckedRules = configs['disable-type-checked'].rules
