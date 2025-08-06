af9cde75c48c99ed711199a91c7d2da79d9b30bf
Jonatas Walker <jonataswalker@gmail.com>
2025-08-06 10:15:38 -0300

    add perfectionist

--- config/formatters.js
+++ config/formatters.js
@@ -3,14 +3,14 @@ import format from 'eslint-plugin-format'
 /** @type {import("eslint").Linter.Config[]} */
 export default [
     {
-        name: 'jwalker:formatters',
-        files: ['**/*.css'],
-        languageOptions: {
-            parser: format.parserPlain,
-        },
-        plugins: { format },
         rules: {
             'format/prettier': ['error', { parser: 'css', tabWidth: 4 }],
         },
+        languageOptions: {
+            parser: format.parserPlain,
+        },
+        name: 'jwalker:formatters',
+        files: ['**/*.css'],
+        plugins: { format },
     },
 ]
--- test/exports.spec.js
+++ test/exports.spec.js
@@ -1,5 +1,5 @@
-import assert from 'node:assert'
 import { describe, it } from 'node:test'
+import assert from 'node:assert'
 
 import * as E from '../config/index.js'
 
