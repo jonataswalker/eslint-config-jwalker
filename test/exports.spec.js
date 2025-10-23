a620607ef70d423cb8c93eec256581e6af46a0aa
Jonatas Walker <jonataswalker@gmail.com>
2025-10-23 10:57:40 -0300

    fix tests

--- test/exports.spec.js
+++ test/exports.spec.js
@@ -3,24 +3,14 @@ import { it, describe } from 'node:test'
 
 import * as E from '../config/index.js'
 
-const isObject = (obj) => Boolean(obj) && obj.constructor === Object
-
 describe('lib exports', () => {
     it('exports', () => {
-        assert.ok(Array.isArray(E.common) && E.common.length > 0)
-
-        assert.ok(Array.isArray(E.common) && E.common.length > 0)
-
         assert.ok(Array.isArray(E.node) && E.node.length > 0)
-
-        assert.ok(Array.isArray(E.typescript) && E.typescript.length > 0)
-
         assert.ok(Array.isArray(E.jsonc) && E.jsonc.length > 0)
-
-        assert.ok(
-            isObject(E.tsDisableTypeCheckedRules)
-            && Object.keys(E.tsDisableTypeCheckedRules).length > 0,
-        )
+        assert.ok(Array.isArray(E.common) && E.common.length > 0)
+        assert.ok(Array.isArray(E.disabled) && E.disabled.length > 0)
+        assert.ok(Array.isArray(E.formatters) && E.formatters.length > 0)
+        assert.ok(Array.isArray(E.typescript) && E.typescript.length > 0)
     })
 
     it('exports constants', () => {
